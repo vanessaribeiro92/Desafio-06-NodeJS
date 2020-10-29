5bea550f0b3a306169d0ed9e292eaa3378cf7c4c
Vanessa Ribeiro <vanessa.ribeiro2492@gmail.com>
2020-10-29 17:04:29 -0300

    add for challenge 7

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
+import cors from 'cors';
 
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
@@ -12,6 +13,7 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
@@ -23,7 +25,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     });
   }
 
-  console.error(err);
+  // console.error(err);
 
   return response.status(500).json({
     status: 'error',
